abd4214496bc55615c83674ea6d8eba416e81f20
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-09 11:23:15 +1100

    added CRUD to backend

--- backend/server.js
+++ backend/server.js
@@ -10,12 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Connect to mongoDB
 const uri = process.env.ATLAS_URI;
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
 const connection = mongoose.connection;
 connection.once('open', () => {
   console.log("MongoDB database connect established successfully.")
 })
+
+
+// Routing
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
