b69578970dc43e32f8c41a488db7228b5df11e3e
robyn <65057146+robynsdev@users.noreply.github.com>
2021-01-09 14:25:55 +1100

    routing done

---
+++ src/components/create-user.component.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export default class CreateUsers extends Component {
+  render() {
+    return <div>
+      <p>You are on create users</p>
+    </div>;
+  }
+}
